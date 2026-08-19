924efac8c25baa1460b81d291ec52348ae3d7e75
mamatha <mamatha0697@gmail.com>
2026-08-19 17:29:41 +0530

    Switch registration mail from PHPMailer/SMTP to PHP mail(); remove unused vendor/composer

--- php/course-registration.php
+++ php/course-registration.php
@@ -1,20 +1,10 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: ../course.php');
     exit;
 }
 
-$configFile = __DIR__ . '/mail-config.php';
-if (!file_exists($configFile)) {
-    http_response_code(500);
-    exit('Mail is not configured yet. Copy php/mail-config.example.php to php/mail-config.php and fill in your SMTP details.');
-}
-$config = require $configFile;
+$toEmail = 'mamatha@rightturn.co.in';
 
 function field(string $key): string
 {
@@ -32,40 +22,16 @@ if ($name === '' || $phone === '' || $email === '' || !filter_var($email, FILTER
     exit('Please fill in all required fields with a valid email address.');
 }
 
-$mail = new PHPMailer(true);
+$subject = 'New Course Registration: ' . $name;
+$body    = "Name: $name\nPhone: $phone\nEmail: $email\nCourse: $course\nMessage: $message";
+$headers = "From: $email\r\n" .
+    "Reply-To: $email\r\n" .
+    "Content-type: text/plain; charset=UTF-8\r\n";
 
-try {
-    $mail->isSMTP();
-    $mail->Host       = $config['smtp_host'];
-    $mail->Port       = $config['smtp_port'];
-    $mail->SMTPAuth   = true;
-    $mail->Username   = $config['smtp_username'];
-    $mail->Password   = $config['smtp_password'];
-    if (!empty($config['smtp_secure'])) {
-        $mail->SMTPSecure = $config['smtp_secure'];
-    }
-
-    $mail->setFrom($config['from_email'], $config['from_name']);
-    $mail->addAddress($config['to_email'], $config['to_name']);
-    $mail->addReplyTo($email, $name);
-
-    $mail->isHTML(true);
-    $mail->Subject = 'New Course Registration: ' . $name;
-    $mail->Body    = sprintf(
-        'Name: %s<br>Phone: %s<br>Email: %s<br>Course: %s<br>Message: %s',
-        htmlspecialchars($name),
-        htmlspecialchars($phone),
-        htmlspecialchars($email),
-        htmlspecialchars($course),
-        nl2br(htmlspecialchars($message))
-    );
-
-    $mail->send();
-
+if (mail($toEmail, $subject, $body, $headers)) {
     header('Location: ../than-you-courseregistration.php');
     exit;
-} catch (Exception $e) {
-    http_response_code(500);
-    echo 'Registration could not be sent. Error: ' . htmlspecialchars($mail->ErrorInfo);
-    exit;
 }
+
+http_response_code(500);
+exit('Registration could not be sent. Please try again later.');
